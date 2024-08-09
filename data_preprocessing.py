6d749e819efbfd0da41064204bef45ee0112f6a2
Laila Azizi <65095793+LailaAzizi@users.noreply.github.com>
2024-08-09 14:40:23 -0700

    Create data_preprocessing.py

---
+++ data_preprocessing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sqlite3
+
+def load_data(database_path):
+    conn = sqlite3.connect(database_path)
+    query = "SELECT * FROM subscription_data;"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+
+def preprocess_data(df):
+    df['signup_date'] = pd.to_datetime(df['signup_date'])
+    df.fillna(0, inplace=True)
+    return df
